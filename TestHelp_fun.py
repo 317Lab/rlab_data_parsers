a47028f60b6f75e34418ab84d0953a3d42d76f92
km4ege <magda14@vt.edu>
2020-12-10 15:44:33 -0500

    Added function that will allow for differently formated times to be input at one time for conversion into time from start in seconds.

--- TestHelp_fun.py
+++ TestHelp_fun.py
@@ -41,6 +41,53 @@ def test_times2seconds(time_lst, laps=True):
     return tsec_lst
 
 
+def rectimes2seconds(time_lst, laps=True, unit='s'):
+    """
+    Convert recorded test times to seconds. 
+
+    Params
+    ----------
+    time_lst    : list/array
+        List of times. Each time in the list can be in list format and/or number format. The format of a time in the list/array depends on that time's type(). 
+        If the time in the list/array time_lst is a(n)
+            --> list/array: its format is either [hh, mm, ss], [mm, ss] or [ss] 
+            --> float/int: its units are specified by the optional input variable "unit"
+    laps        : bool
+        (Optional) Whether or not the times in time_lst are stopwatch lap times (True) or times measured from time zero (False). 
+        [Default: laps=True]
+    units       : str
+        (Optional) A character only used if one or more entries in time_lst are of type float/int (i.e. not a sub-list/array), to indicate the units of such entries.
+        Possible values: 
+            --> 's': [Default] Seconds
+            --> 'm': Minutes
+            --> 'h': Hours
+
+    Returns
+    --------
+    tsec_lst    : list
+        List of times in seconds from time zero.
+
+    """
+#    minu, sec = time_lst[0]
+
+    tsec_lst = []
+    for rec in time_lst: 
+        if hasattr(rec, '__iter__'): 
+            if len(rec)==3: rec = rec[0]*60**2 +rec[1]*60+rec[2]
+            elif len(rec)==2: rec = rec[0]*60+rec[1]
+            elif len(rec)==1: rec = rec[0]
+            else: print 'ERROR: Unrecognized Time Format!'
+        elif not unit=='s': 
+            if unit=='m': rec = rec*60
+            elif unit=='h': rec = rec*60**2
+            else: print 'ERROR: Unrecognized Time Unit!'
+
+        if laps and 0<len(tsec_lst):
+            tsec_lst.append(rec+tsec_lst[-1])
+        else: 
+            tsec_lst.append(rec)
+    return tsec_lst
+
 # def lap2totalElapsed_time(time_lst, input_units='s'): 
 #     if hasattr(time_lst[0], '__itter__'): 
 #         ##If of format [mm:ss], then 
