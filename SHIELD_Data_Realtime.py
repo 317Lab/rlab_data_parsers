aa8102120002ddd932109507b63df61efe0dd124
Jules van Irsel <jules.van.irsel.gr@dartmouth.edu>
2022-01-10 17:08:29 -0500

    added plotting history

--- SHIELD_Data_Realtime.py
+++ SHIELD_Data_Realtime.py
@@ -3,13 +3,14 @@
 # Arguments: 1) Portname, 2) Data filename suffix (Optional)
 # Writes all captured bytes to bin file in daemonic thread
 # Parses and plots in set intervals
-# 10 minute initial timeout to allow for early code execution
-# Loop terminates after a 5 minute timeout with no data after initial full frame capture
-# To stop recording press Enter or close figure.
+# 15 minute initial timeout to allow for early code execution
+# Loop terminates after a 10 minute timeout with no data after initial full frame capture
+# To stop recording close figure.
 # Contact: jules.van.irsel.gr@dartmouth.edu
 
 import sys
 import os
+from numpy.lib.arraysetops import in1d
 import serial
 from datetime import datetime
 import numpy as np
@@ -33,13 +34,16 @@ if len(sys.argv)==3: # optional suffix argument
     suffix = '_' + sys.argv[2]
 else:
     suffix = ''
-ser = serial.Serial(port, baud, timeout=600) # wait until all bytes are read or x seconds pass
+ser = serial.Serial(port, baud, timeout=900) # wait until all bytes are read or x seconds pass
 ser.reset_input_buffer() # flush serial port
 fn = datetime.now().strftime("%Y%m%dT%H%M%S") + '_data_' + port.split('.')[-1] + '_' + str(baud) + suffix + '.bin'
 f = open(fn,'ab')
 fig, axs = plt.subplots(6, 1, figsize=(16,9))
+
+# initialize global params
 plotting = True
 rawBytes = b'\x00'
+payloadID = 0
 
 # for closing on figure exit
 def on_close(event):
@@ -60,17 +64,31 @@ def conc(word):
 #     plotting = False
 
 def read_write_thread():
-    global plotting, rawBytes
+    global plotting, rawBytes, payloadID
     while plotting:
         rawBytes = ser.read(numBytesTarget)
         f.write(rawBytes)
     f.close()
+    os.rename(fn,fn[:-4]+'_'+str(payloadID)+'.bin') # add payload ID to filename
 
 # start main loop
 def main():
-    global plotting, rawBytes
+    global plotting, rawBytes, payloadID
     # th.Thread(target=key_capture_thread, args=(), daemon=False).start() # thread awaiting input
-    th.Thread(target=read_write_thread, args=(), daemon=True).start() # thread reading and writing bytes
+    th.Thread(target=read_write_thread, args=(), name='read_write_thread', daemon=False).start() # thread reading and writing raw bytes
+    
+    # initialize history arrays
+    SWPTimeOld = np.zeros(0,dtype='uint32') # unsigned 4 bytes
+    payloadIDOld = np.zeros(0,dtype='uint8') # unsigned 1 byte
+    pip0VoltagesOld = np.zeros(0,dtype='int16') # signed 2 bytes
+    pip1VoltagesOld = np.zeros(0,dtype='int16')
+    IMUTimeOld = np.zeros(0,dtype='uint32')
+    axOld = np.zeros(0,dtype='int16'); ayOld = np.zeros(0,dtype='int16'); azOld = np.zeros(0,dtype='int16') 
+    mxOld = np.zeros(0,dtype='int16'); myOld = np.zeros(0,dtype='int16'); mzOld = np.zeros(0,dtype='int16')
+    gxOld = np.zeros(0,dtype='int16'); gyOld = np.zeros(0,dtype='int16'); gzOld = np.zeros(0,dtype='int16')
+    IMUTempOld = np.zeros(0,dtype='int16')
+    IMUCadOld = np.zeros(0,dtype='uint32')
+
     while plotting:
         # rawBytes = ser.read(numBytesTarget)
         # f.write(rawBytes)
@@ -79,7 +97,7 @@ def main():
             print('No data after serial timeout',flush=True)
             plotting = False
         elif numBytes == numBytesTarget:
-            ser.timeout = plotTime*10 # shorten timeout after initial message capture
+            ser.timeout = 600 # shorten timeout after initial message capture
             numDataSWP = round(plotTime*freq*numSamples*2.5) # add 250% for safety
             numDataIMU = round(plotTime*freq*2.5)
 
@@ -129,11 +147,11 @@ def main():
 
             if posSWP==0 or posIMU==0: # no full messages found indicating scrambled bytes
                 print('DATA DROPOUT AT ' + datetime.now().strftime("%Y%m%dT%H%M%S"),flush=True)
-                time.sleep(1) # only print in 1 second intervals
+                time.sleep(1) # print in 1 second intervals until end of data drop
             else:
                 # Converting bytes and scaling data
                 SWPTime = SWPTime[0:posSWP]*tScale # chop off unused zeros
-                payloadID = payloadID[0:posSWP]
+                payloadID = payloadID[0]
                 pip0Voltages = pip0Voltages[0:posSWP]*pScale
                 pip1Voltages = pip1Voltages[0:posSWP]*pScale
                 IMUTime = IMUTime[0:posIMU]*tScale
@@ -144,69 +162,82 @@ def main():
 
                 IMUCad = np.diff(IMUTime)*1e3
                 IMUCad = np.append(IMUCad,IMUCad[-1]) # make array same length
-                pip0rms = np.sqrt(np.mean(np.square(pip0Voltages-np.mean(pip0Voltages))))*1e3 # determine rms
+                pip0rms = np.sqrt(np.mean(np.square(pip0Voltages-np.mean(pip0Voltages))))*1e3 # calculate rms
                 pip1rms = np.sqrt(np.mean(np.square(pip1Voltages-np.mean(pip1Voltages))))*1e3
-                pip0std = np.std(pip0Voltages)*1e3 # determine standard deviation
+                pip0std = np.std(pip0Voltages)*1e3 # calculate standard deviation
                 pip1std = np.std(pip1Voltages)*1e3
 
                 # Plotting
                 fig.canvas.mpl_connect('close_event', on_close) # exit loop when closing figure
                 fig.subplots_adjust(hspace=0)
+                lw = 1
                 
                 axs[0].clear()
-                axs[0].plot(IMUTime,ax)
-                axs[0].plot(IMUTime,ay)
-                axs[0].plot(IMUTime,az)
+                axs[0].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((axOld,ax)),linewidth=lw)
+                axs[0].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((ayOld,ay)),linewidth=lw)
+                axs[0].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((azOld,az)),linewidth=lw)
                 axs[0].set_ylabel('ACC [g]')
                 axs[0].grid()
+                axs[0].ticklabel_format(useOffset=False)
                 axs[0].set_xlabel('IMU TIME SINCE SHIELD POWER [s]')
                 axs[0].xaxis.tick_top()
                 axs[0].xaxis.set_label_position('top')
-                axs[0].text(0.9, 1.5, 'SHIELD ID: ' + str(payloadID[1]), transform=axs[0].transAxes)
+                axs[0].text(0.9, 1.5, 'SHIELD ID: ' + str(payloadID), transform=axs[0].transAxes)
 
                 axs[1].clear()
-                axs[1].plot(IMUTime,mx)
-                axs[1].plot(IMUTime,my)
-                axs[1].plot(IMUTime,mz)
+                axs[1].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((mxOld,mx)),linewidth=lw)
+                axs[1].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((myOld,my)),linewidth=lw)
+                axs[1].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((mzOld,mz)),linewidth=lw)
                 axs[1].set_ylabel('MAG [G]')
                 axs[1].grid()
+                axs[1].ticklabel_format(useOffset=False)
                 axs[1].xaxis.set_ticklabels([])
 
                 axs[2].clear()
-                axs[2].plot(IMUTime,gx)
-                axs[2].plot(IMUTime,gy)
-                axs[2].plot(IMUTime,gz)
+                axs[2].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((gxOld,gx)),linewidth=lw)
+                axs[2].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((gyOld,gy)),linewidth=lw)
+                axs[2].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((gzOld,gz)),linewidth=lw)
                 axs[2].set_ylabel('GYR [Hz]')
                 axs[2].grid()
+                axs[2].ticklabel_format(useOffset=False)
                 axs[2].xaxis.set_ticklabels([])
-
+                
                 axs[3].clear()
-                axs[3].plot(IMUTime,IMUCad)
+                axs[3].plot(np.concatenate((IMUTimeOld,IMUTime)),np.concatenate((IMUCadOld,IMUCad)),linewidth=lw/2)
                 axs[3].set_ylabel('CAD [ms]')
                 axs[3].grid()
                 axs[3].ticklabel_format(useOffset=False)
                 axs[3].xaxis.set_ticklabels([])
-
+                
                 axs[4].clear()
-                axs[4].plot(SWPTime[1:],pip0Voltages[1:])
+                axs[4].plot(np.concatenate((SWPTimeOld,SWPTime[1:])),np.concatenate((pip0VoltagesOld,pip0Voltages[1:])),linewidth=lw/2)
                 axs[4].set_ylabel('P0 [V]')
                 axs[4].grid()
+                axs[4].ticklabel_format(useOffset=False)
                 axs[4].xaxis.set_ticklabels([])
 
                 axs[5].clear()
-                axs[5].plot(SWPTime[1:],pip1Voltages[1:])
+                axs[5].plot(np.concatenate((SWPTimeOld,SWPTime[1:])),np.concatenate((pip1VoltagesOld,pip1Voltages[1:])),linewidth=lw)
                 axs[5].set_ylabel('P1 [V]')
                 axs[5].grid()
+                axs[5].ticklabel_format(useOffset=False)
                 axs[5].set_xlabel('SWEEP TIME SINCE SHIELD POWER [s]')
                 axs[5].text(-0.1, -0.6, 'P0 RMS: ' + "{0:.1f}".format(pip0rms) + ' mV', transform=axs[5].transAxes)
                 axs[5].text(-0.1, -0.8, 'P1 RMS: ' + "{0:.1f}".format(pip1rms) + ' mV', transform=axs[5].transAxes)
                 axs[5].text( 0.1, -0.6, 'P0 STD: ' + "{0:.1f}".format(pip0std) + ' mV', transform=axs[5].transAxes)
                 axs[5].text( 0.1, -0.8, 'P1 STD: ' + "{0:.1f}".format(pip1std) + ' mV', transform=axs[5].transAxes)
 
-                plt.pause(0.1) # needed for pyplot realtime plotting. might switch back to pyqtgraph for speed
+                SWPTimeOld = SWPTime
+                payloadIDOld = payloadID
+                pip0VoltagesOld = pip0Voltages
+                pip1VoltagesOld =pip1Voltages
+                IMUTimeOld = IMUTime
+                axOld = ax; ayOld = ay; azOld = az
+                mxOld = mx; myOld = my; mzOld = mz
+                gxOld = gx; gyOld = gy; gzOld = gz
+                IMUTempOld = IMUTemp
+                IMUCadOld = IMUCad
 
-    plt.show()
-    time.sleep(1)
-    # os.rename(fn,fn[:-4]+'_'+str(payloadID[-1])+'.bin') # add payload ID to filename
+                plt.pause(plotTime) # needed for pyplot realtime plotting. might switch back to pyqtgraph
 
-main()
+main() # run main loop
